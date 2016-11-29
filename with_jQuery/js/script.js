1e38f523df86bec29b2a935c52546734fcdc0e3c
Przemek Pamirski <ppamirski@gmail.com>
2016-11-29 19:50:07 +0100

    Chuck Norris joke’s generator with jQuery

---
+++ with_jQuery/js/script.js
@@ -0,0 +1,14 @@
+var url = 'http://api.icndb.com/jokes/random';
+var $button = $('#get-joke').click(function() {
+	getJoke();
+});
+getJoke();
+var $paragraph = $('#joke');
+function getJoke() {
+$.ajax({
+	method: 'GET', url: url,
+	success: function(res) {
+		$paragraph.text(res.value.joke);
+	}
+});
+}
